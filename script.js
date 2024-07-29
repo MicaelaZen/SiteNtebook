f34ff7afd0f4e2e159941fb9e29e6b2b138a243f
Micaela Zen <garciamicaela@gmail.com>
2024-07-29 00:16:01 -0300

    incomplet com inicio do API

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ document.getElementById('cepForm').addEventListener('submit', function(event) {
     event.preventDefault();
   
   
-    const notebook = document.getElementById('notebook').value;
+    const notebooks = document.getElementById('notebook').value;
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=notebooks`;
-  
+
+    for{
+        const title1 = "Notebook Gamer Acer Predator Intel Core I7 13650hx 16gb Ram 1tb Ssd Nvidia Geforce Rtx 4060 Windows11 Home Tela 16 Phn16-71-709k Preto";
+        fetch (url)
+        then (response => response.json())
+        then (data => {
+            document.getElementById('id').textContent = title1;
+        })
+    }
